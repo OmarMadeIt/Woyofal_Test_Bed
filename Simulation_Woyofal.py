7497fcd0d991c0ae74189cb60227cb009d2d542c
OmarMadeIt <106669673+OmarMadeIt@users.noreply.github.com>
2023-08-01 20:59:26 +0000

    Add files via upload

--- Simulation_Woyofal.py
+++ Simulation_Woyofal.py
@@ -51,9 +51,9 @@ def calculate_nb_watts(montant_encours, montant_nouveau, seuil_1, seuil_2, Prix_
     montant_tranche_2_HT = montant_tranche_2-montant_tranche_2*Taxe_Com
     montant_tranche_3_HT = montant_tranche_3-montant_tranche_3*Taxe_Com-montant_tranche_3*TVA
 
-    conso_tranche_1 = montant_tranche_1_HT/Prix_tranche_1
-    conso_tranche_2 = montant_tranche_2_HT/Prix_tranche_2
-    conso_tranche_3 = montant_tranche_3_HT/Prix_tranche_3
+    conso_tranche_1 = round(montant_tranche_1_HT/Prix_tranche_1,2)
+    conso_tranche_2 = round(montant_tranche_2_HT/Prix_tranche_2,2)
+    conso_tranche_3 = round(montant_tranche_3_HT/Prix_tranche_3,2)
     nb_watts = conso_tranche_1 + conso_tranche_2 + conso_tranche_3
     return nb_watts, conso_tranche_1, conso_tranche_2, conso_tranche_3
 
@@ -78,10 +78,10 @@ elif foyer_type == "DMP (Domestique Moyenne Puissance)":
     
 st.write("Veuillez préciser les renseignements ci-dessous :")
 #montant_encours = st.number_input("Quel est le montant total que vous avez déjà rechargé au cours de ce mois ?", value=0, step=1)
-montant_encours = st.slider("Quel est le montant total que vous avez déjà rechargé au cours de ce mois ?", min_value=0, max_value=100000, step=1000, value=0)
+montant_encours = st.slider("Quel est le montant total que vous avez déjà rechargé au cours de ce mois ?", min_value=0, max_value=100000, step=500, value=0)
 
 #montant_nouveau = st.number_input("Combien souhaitez-vous recharger ?", value=0, step=1)
-montant_nouveau = st.slider("Combien souhaitez-vous recharger ?", min_value=0, max_value=100000, step=1000, value=0)
+montant_nouveau = st.slider("Combien souhaitez-vous recharger ?", min_value=0, max_value=100000, step=500, value=0)
 
 if st.button("Calculer"):
     nb_watts, conso_tranche_1, conso_tranche_2, conso_tranche_3=calculate_nb_watts(montant_encours,montant_nouveau, seuil_1, seuil_2, Prix_tranche_1, Prix_tranche_2, Prix_tranche_3)
